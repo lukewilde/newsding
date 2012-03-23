4892dfc1f2f9265f61f82ced26994fb84e9ca90a
Luke Wilde <luke.wilde@clock.co.uk>
2012-03-23 22:39:05 +0000

    Adding support for categories.

--- js/newsding.js
+++ js/newsding.js
@@ -5,18 +5,20 @@ $(function(){
       base = this,
       options = {
         numRows: localStorage.numrows,
-        sources: $.parseJSON(localStorage.sources).data
+        categories: $.parseJSON(localStorage.sources).categories
       };
 
-      // console.log(this.url);
-    $(options.sources).each(function() {
-      $.getFeed({
-        url: this.url,
-        success: function(feed) {
-          console.log(feed);
-        }
+    $(options.categories).each(function() {
+      $(this.items).each(function() {
+
+         console.log($(this));
+        // $.getFeed({
+        //   url: this.url,
+        //   success: function(feed) {
+        //     console.log(feed);
+        //   }
+        // });
       });
     });
-
   };
 }(jQuery));
