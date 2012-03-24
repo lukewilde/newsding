efe93f6ba9a89d01f18f5fb8b65c88217ec1ba91
Luke Wilde <luke.wilde@clock.co.uk>
2012-03-24 03:00:33 +0000

    Adding output to page and experimenting with localStorage.

--- js/newsding/newsding.js
+++ js/newsding/newsding.js
@@ -3,21 +3,71 @@ $(function(){
   $.fn.newsding = function() {
     var
       base = this,
+      categores = null,
       options = {
         numRows: localStorage.numrows,
         categories: $.parseJSON(localStorage.sources).categories
       };
 
-    $(options.categories).each(function() {
-      $(this.items).each(function() {
-        console.log("woo");
-        // $.ajax({
-        //   url: this.url,
-        //   success: function(feed) {
-        //     console.log(this, feed);
-        //   }
-        // });
+    // init();
+    downloadCategories();
+
+    function init() {
+
+      $.parseJSON(localStorage.categories);
+
+      $(categories).each(function() {
+        renderSource(this);
       });
-    });
+    }
+
+    function renderSource(source) {
+
+      source.each(function() {
+        var title = $(this).find("title").text();
+        var description = $(this).find("description").text();
+        var image = $(description).find("img");
+
+        $("body").append("<h3>" + title + "<h3>");
+        $("body").append(image);
+      });
+    }
+
+    function downloadCategories () {
+      $(options.categories).each(function() {
+
+        var category = this;
+        var sources = {};
+
+        $(this.items).each(function() {
+          $.ajax({
+            url: this.url,
+            settings : {
+              dataType : "xml"
+            },
+            success: function(feed) {
+
+              if (typeof localStorage.categories == "undefined") {
+                localStorage.categories = {};
+              }
+
+              $feed = $(feed).find("item");
+
+              // Persistance in localStorage
+              localStorage.categories[this.url] = $feed.text();
+              console.log(localStorage.categories);
+              // Persistance in memory
+              categories[this.url] = $feed;
+
+              // console.log(this.url);
+
+              // Update view.
+              renderSource($feed);
+            }
+          });
+        });
+      });
+    }
+
   };
 });
