75d339ed57e44bd42cef0d3280e5cf6dab147724
Luke Wilde <luke.wilde@clock.co.uk>
2012-03-24 16:54:18 +0000

    Ensuring jQuery loads first.

---
+++ js/newsding/config.js
@@ -0,0 +1,6 @@
+// Set the require.js configuration for your application.
+require.config({
+  // Initialize the application with the main application file
+  deps: ['main'],
+  priority: ['/js/lib/jquery-1.7.1.min.js']
+});
--- js/newsding/main.js
+++ js/newsding/main.js
@@ -1,5 +1,4 @@
 require([
-  '/js/lib/jquery-1.7.1.min.js',
   '/js/lib/jquery.tmpl.min.js',
   '/js/lib/jquery-jfeed.js',
   '/js/newsding/newsding.js'
